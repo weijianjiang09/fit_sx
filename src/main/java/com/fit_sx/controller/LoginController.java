b9beff7a8807d0dc4b582af208923415a9db962a
weijianjiang09 <jiangweijian09@qq.com>
2022-06-12 11:25:09 +0800

    修正

--- src/main/java/com/fit_sx/controller/LoginController.java
+++ src/main/java/com/fit_sx/controller/LoginController.java
@@ -3,23 +3,23 @@ package com.fit_sx.controller;
 import com.fit_sx.model.User;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(name = "login", value = "/login")
 public class LoginController extends BaseController{
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-        String method = req.getParameter("method");
-        if (method.equals("add")){
-            User user = getBean(User.class,req);
-            success(resp,"",user);
-        }
+        resp.setContentType("application/json;charset=utf-8");
+        User user = getBean(User.class,req);
+        success(resp,"登录成功！",user);
+        System.out.println(user);
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        this.doPost(req, resp);
+        doPost(req, resp);
     }
 }
--- src/main/webapp/back/js/login.js
+++ src/main/webapp/back/js/login.js
@@ -1,16 +1,28 @@
 $(document).ready(function () {
     let form = layui.form;
     form.on('submit(login)', function(data){
+        var index
         $.ajax({
+            url:config.url+"/login",
             method:"post",
-            data:data,
+            data:data.field,
+            timeout:3000,
             success:function (res) {
+                index =layer.load(2, {time: 2*1000})
                 if(res.code==1){
                     layer.msg(res.msg);
-                    location.href="";
+                    console.log(res.data)
+                    // location.href="";
+                    layer.close(index)
                 }else {
                     layer.msg(res.msg);
+                    layer.close(index)
                 }
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log("ss")
+                layer.msg("请求超时");
+                layer.close(index)
             }
         })
         return false;
